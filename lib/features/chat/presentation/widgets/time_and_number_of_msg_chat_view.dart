6570a95c726fe0552a0fcf9986f91a590ee9d17c
esraaehab333 <esraaehabb333@gmail.com>
2024-12-24 04:27:31 +0200

    Build Chat Card and interior details ans Build Nochat view

---
+++ lib/features/chat/presentation/widgets/time_and_number_of_msg_chat_view.dart
@@ -0,0 +1,43 @@
+import 'package:aggar/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class TimeAndNumberOfMsgChatView extends StatelessWidget {
+  const TimeAndNumberOfMsgChatView({
+    super.key,
+    required this.time,
+    required this.numberMsg,
+  });
+  final String time;
+  final int numberMsg;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          time,
+          style: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+            color: AppColors.myGray100_3,
+          ),
+        ),
+        const Gap(2),
+        Container(
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            color: AppColors.myBlue100_3,
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Center(
+            child: Text(
+              numberMsg.toString(),
+              style: const TextStyle(fontSize: 12),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
