dadb4b222aadc8ea0531b6c049936b6a5fb7c023
esraaehab333 <esraaehabb333@gmail.com>
2025-01-16 23:04:34 +0200

    add language card with the switch and dark mode card with the switch

---
+++ lib/features/settings/presentation/widgets/dark_mode_card.dart
@@ -0,0 +1,44 @@
+import 'package:aggar/core/utils/app_assets.dart';
+import 'package:aggar/core/utils/app_colors.dart';
+import 'package:aggar/features/settings/presentation/views/settings_screen.dart';
+import 'package:aggar/features/settings/presentation/widgets/custom_card_settings_page.dart';
+import 'package:aggar/features/settings/presentation/widgets/dark_mode_switch.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class DarkModeCard extends StatelessWidget {
+  const DarkModeCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomCardSettingsPage(
+      padingHorizental: 5,
+      padingVeritical: 3,
+      backgroundColor: AppColors.myBlue100_7,
+      child: Row(
+        children: [
+          const Image(
+            image: AssetImage(
+              AppAssets.assetsIconsDarkMode,
+            ),
+            height: 25,
+            width: 25,
+          ),
+          const Gap(10),
+          Text(
+            "Dark Mode",
+            style: TextStyle(
+              fontSize: 14,
+              color: AppColors.myBlue100_1,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(),
+          const DarkModeSwitch()
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/settings/presentation/widgets/language_switch.dart
@@ -0,0 +1,42 @@
+import 'package:aggar/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+
+class LanguageSwitch extends StatelessWidget {
+  const LanguageSwitch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 4,
+          vertical: 4,
+        ),
+        decoration: BoxDecoration(
+          color: AppColors.myBlue100_1,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(
+              10,
+            ),
+          ),
+        ),
+        child: ToggleSwitch(
+          fontSize: 10,
+          minWidth: 35,
+          minHeight: 20,
+          radiusStyle: true,
+          initialLabelIndex: 1,
+          totalSwitches: 2,
+          labels: const ['EN', 'AR'],
+          onToggle: (index) {},
+          activeBgColor: [AppColors.myWhite100_1],
+          activeFgColor: AppColors.myBlue100_1,
+          inactiveBgColor: AppColors.myBlue100_1,
+          inactiveFgColor: AppColors.myWhite100_1,
+        ),
+      ),
+    );
+  }
+}
