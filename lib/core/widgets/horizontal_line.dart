b36d1899fbf763f2370eaacd9b54d091d61d497d
esraaehab333 <esraaehabb333@gmail.com>
2025-01-16 19:46:17 +0200

    add payment card and rent history

---
+++ lib/core/widgets/horizontal_line.dart
@@ -0,0 +1,21 @@
+import 'package:aggar/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HorizontalLine extends StatelessWidget {
+  const HorizontalLine({
+    super.key,
+    this.padding,
+    this.color,
+  });
+  final double? padding;
+  final Color? color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: padding ?? 15),
+      child: Divider(
+        color: color ?? AppColors.myBlack10,
+      ),
+    );
+  }
+}
--- lib/features/settings/presentation/views/settings_screen.dart
+++ lib/features/settings/presentation/views/settings_screen.dart
@@ -1,6 +1,10 @@
+import 'package:aggar/core/widgets/horizontal_line.dart';
 import 'package:aggar/features/settings/presentation/widgets/custom_card_settings_page.dart';
 import 'package:aggar/features/settings/presentation/widgets/image_with_name_and_email_section.dart';
+import 'package:aggar/features/settings/presentation/widgets/payment_card.dart';
+import 'package:aggar/features/settings/presentation/widgets/rent_history_card.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import '../../../../core/utils/app_colors.dart';
 
@@ -20,10 +24,19 @@ class SettingsScreen extends StatelessWidget {
         child: Column(
           children: [
             CustomCardSettingsPage(
+              padingHorizental: 0,
+              padingVeritical: 12,
+              onPressed: () {},
               borderColor: AppColors.myBlue100_1,
               backgroundColor: Colors.transparent,
               child: const ImageWithNameAndEmailSection(),
             ),
+            const Gap(12),
+            const PaymentCard(),
+            const Gap(12),
+            const RentHistoryCard(),
+            const Gap(5),
+            const HorizontalLine(),
           ],
         ),
       ),
