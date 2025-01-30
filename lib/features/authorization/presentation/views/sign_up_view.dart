4c5ead722f9664a6fa4af6803640559ac11e1263
esraaehab333 <esraaehabb333@gmail.com>
2025-01-30 05:51:29 +0200

    refactor: redesign sign-up view with added font

--- lib/features/authorization/presentation/views/sign_up_view.dart
+++ lib/features/authorization/presentation/views/sign_up_view.dart
@@ -1,10 +1,11 @@
-import 'package:aggar/core/helper/get_font_size.dart';
 import 'package:aggar/core/utils/app_colors.dart';
-import 'package:aggar/core/widgets/custom_elevated_button.dart';
-import 'package:aggar/features/authorization/presentation/widget/custom_text_from_felid.dart';
+import 'package:aggar/features/authorization/presentation/widget/sign_up_all_fields.dart';
+import 'package:aggar/features/authorization/presentation/widget/sign_up_have_an_account_section.dart';
+import 'package:aggar/features/authorization/presentation/widget/sign_up_next_button.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../core/utils/app_styles.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
@@ -21,84 +22,13 @@ class SignUpView extends StatelessWidget {
           children: [
             Text(
               "Let's sign you up",
-              style: GoogleFonts.inter(
-                color: AppColors.myBlack100,
-                fontSize: getFontSize(context, 24),
-                fontWeight: FontWeight.bold,
-              ),
+              style: AppStyles.bold28(context),
             ),
-            const CustomTextField(
-              labelText: 'Name',
-              inputType: TextInputType.text,
-              obscureText: false,
-              hintText: "Say my name",
-            ),
-            const CustomTextField(
-              labelText: 'Date of birth',
-              inputType: TextInputType.text,
-              obscureText: false,
-              hintText: "Enter your age",
-            ),
-            const CustomTextField(
-              labelText: 'Email',
-              inputType: TextInputType.text,
-              obscureText: false,
-              hintText: "Enter your Email",
-            ),
-            const CustomTextField(
-              labelText: 'Password',
-              inputType: TextInputType.text,
-              obscureText: false,
-              hintText: "Enter password",
-              suffixIcon: Icon(Icons.visibility_off),
-            ),
-            const Gap(65),
-            Center(
-              child: CustomElevatedButton(
-                borderRadius: BorderRadius.circular(15),
-                color: AppColors.myBlue100_1,
-                height: 60,
-                width: 320,
-                onPressed: () {},
-                child: Text(
-                  "Next",
-                  style: GoogleFonts.inter(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.myWhite100_1,
-                  ),
-                ),
-              ),
-            ),
+            const SignUpAllFields(),
+            const Gap(25),
+            const SignUpNextButton(),
             const Gap(5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Have an account?"),
-                const Gap(5),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(
-                      context,
-                    );
-                  },
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                    minimumSize: Size.zero,
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  ),
-                  child: Text(
-                    "Login now",
-                    //TODO : this too will put in app_styles.dart file
-                    style: GoogleFonts.inter(
-                      fontSize: getFontSize(context, 12),
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.myBlue100_1,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            const SignUpHaveAnAccountSection(),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:aggar/features/authorization/presentation/views/pick_image.dart';
 import 'package:aggar/features/authorization/presentation/views/sign_in_view.dart';
+import 'package:aggar/features/authorization/presentation/views/sign_up_view.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      home: const SignInView(),
+      home: const SignUpView(),
     );
   }
 }
