ba108d933285a80c03d096009f62e9ded28afffd
esraaehab333 <esraaehabb333@gmail.com>
2025-02-23 22:15:02 +0200

    refactor: solve swapping page in TabBarSection and convert it to StatefulWidget

--- lib/features/vehicles_details/presentation/widgets/tab_bar_section.dart
+++ lib/features/vehicles_details/presentation/widgets/tab_bar_section.dart
@@ -1,54 +1,80 @@
+import 'package:flutter/material.dart';
 import 'package:aggar/core/utils/app_colors.dart';
 import 'package:aggar/core/utils/app_styles.dart';
 import 'package:aggar/features/vehicles_details/presentation/views/about_tab_bar/views/about_tab_bar_view.dart';
 import 'package:aggar/features/vehicles_details/presentation/views/properities_tab_bar/views/properities_tab_bar_view.dart';
 import 'package:aggar/features/vehicles_details/presentation/views/review_tab_bar/views/review_tab_bar_view.dart';
-import 'package:flutter/material.dart';
 
-class TabBarSection extends StatelessWidget {
+class TabBarSection extends StatefulWidget {
   const TabBarSection({super.key});
 
   @override
+  _TabBarSectionState createState() => _TabBarSectionState();
+}
+
+class _TabBarSectionState extends State<TabBarSection>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Column(
-        children: [
-          TabBar(
-            padding: EdgeInsets.zero,
-            indicatorPadding: EdgeInsets.zero,
-            indicatorColor: AppColors.myBlue100_2,
-            dividerColor: AppColors.myBlack10,
-            labelColor: AppColors.myBlue100_2,
-            unselectedLabelColor: AppColors.myGray100_2,
-            labelStyle: AppStyles.bold18(context)
-                .copyWith(color: AppColors.myBlue100_2),
-            unselectedLabelStyle:
-                AppStyles.bold18(context).copyWith(color: AppColors.myBlack25),
-            tabs: const [
-              Tab(
-                child: Text("About"),
-              ),
-              Tab(
-                child: Text("Properties"),
-              ),
-              Tab(
-                child: Text("Reviews"),
-              ),
-            ],
-          ),
-          const SizedBox(
+    return Column(
+      children: [
+        TabBar(
+          controller: _tabController,
+          padding: EdgeInsets.zero,
+          indicatorPadding: EdgeInsets.zero,
+          indicatorColor: AppColors.myBlue100_2,
+          dividerColor: AppColors.myBlack10,
+          labelColor: AppColors.myBlue100_2,
+          unselectedLabelColor: AppColors.myGray100_2,
+          labelStyle:
+              AppStyles.bold18(context).copyWith(color: AppColors.myBlue100_2),
+          unselectedLabelStyle:
+              AppStyles.bold18(context).copyWith(color: AppColors.myBlack25),
+          tabs: const [
+            Tab(
+              child: Text("About"),
+            ),
+            Tab(
+              child: Text("Properties"),
+            ),
+            Tab(
+              child: Text("Reviews"),
+            ),
+          ],
+          onTap: (index) {
+            _pageController.jumpToPage(index); // Jump to the selected page
+          },
+        ),
+        SizedBox(
             height: 500,
-            child: TabBarView(
-              children: [
+            child: PageView(
+              controller: _pageController,
+              physics:
+                  const NeverScrollableScrollPhysics(), // Disable swipe gestures
+              children: const [
                 Center(child: AboutTabBarView()),
                 Center(child: ProperitiesTabBarView()),
                 Center(child: ReviewTabBarView()),
               ],
-            ),
-          ),
-        ],
-      ),
+            )),
+      ],
     );
   }
 }
