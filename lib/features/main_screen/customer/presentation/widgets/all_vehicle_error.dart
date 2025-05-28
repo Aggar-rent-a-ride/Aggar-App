473f22b522e8d157943ac7dbb9a8837ca35e4e56
esraaehab333 <esraaehabb333@gmail.com>
2025-05-28 23:54:13 +0300

    feat: end of get vehicles , get most rented and popular vehicles endpoint with paginations

---
+++ lib/features/main_screen/customer/presentation/widgets/all_vehicle_error.dart
@@ -0,0 +1,66 @@
+import 'package:aggar/core/extensions/context_colors_extension.dart';
+import 'package:aggar/core/utils/app_constants.dart';
+import 'package:aggar/core/utils/app_styles.dart';
+import 'package:aggar/features/main_screen/customer/presentation/cubit/vehicles/vehicle_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class AllVehicleError extends StatelessWidget {
+  const AllVehicleError({
+    super.key,
+    required this.cubit,
+  });
+
+  final VehicleCubit cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: AppConstants.myRed100_1.withOpacity(0.1),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.error_outline_rounded,
+                size: 64,
+                color: AppConstants.myRed100_1,
+              ),
+            ),
+            const Gap(28),
+            Text(
+              "Oops! Something went wrong",
+              style: AppStyles.bold20(context).copyWith(
+                color: context.theme.black100,
+              ),
+            ),
+            const Gap(10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                  label: const Text("Go Back"),
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: context.theme.gray100_1,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 12),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/main_screen/customer/presentation/widgets/no_vehilce_found_body.dart
@@ -0,0 +1,45 @@
+import 'package:aggar/core/extensions/context_colors_extension.dart';
+import 'package:aggar/core/utils/app_styles.dart';
+import 'package:aggar/features/main_screen/customer/presentation/cubit/vehicles/vehicle_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class NoVehilceFoundBody extends StatelessWidget {
+  const NoVehilceFoundBody({
+    super.key,
+    required this.cubit,
+  });
+
+  final VehicleCubit cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.directions_car_outlined,
+            size: 80,
+            color: context.theme.gray100_1,
+          ),
+          const Gap(28),
+          Text(
+            "No vehicles available",
+            style: AppStyles.bold20(context).copyWith(
+              color: context.theme.black100,
+            ),
+          ),
+          const Gap(8),
+          Text(
+            "We couldn't find any vehicles at the moment.\nPlease try again later.",
+            style: AppStyles.regular16(context).copyWith(
+              color: context.theme.gray100_1,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
